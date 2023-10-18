05cdc0ce9ef8f53551b15eece026591bdebbc9a0
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2023-10-18 12:32:21 +0100

    removing old testing examples

--- examples/clipboard.rs
+++
@@ -1,8 +0,0 @@
-use ad::util::{read_clipboard, set_clipboard};
-
-fn main() {
-    println!("{}", read_clipboard().unwrap());
-    set_clipboard("this is a test").unwrap();
-    std::thread::sleep(std::time::Duration::from_millis(300));
-    println!("{}", read_clipboard().unwrap());
-}
--- examples/external_commands.rs
+++
@@ -1,6 +0,0 @@
-use ad::util::pipe_through_command;
-
-fn main() {
-    let s = pipe_through_command("wc", std::iter::empty::<&str>(), "this is a string").unwrap();
-    println!("OUTPUT: '{s}'");
-}
