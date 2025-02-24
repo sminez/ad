a778457afea548a283d9eb9e7fba65c1353bd102
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-02-24 19:55:42 +0000

    making the async/await state machine stuff look semi-respectable

--- crates/ninep/examples/async_fsm.rs
+++ crates/ninep/examples/async_fsm.rs
@@ -1,34 +1,87 @@
 //! This is a little exploration of using async/await + a dummy Waker to simplify writing sans-io
 //! state machine code.
-use futures::pending;
 use std::{
     cell::RefCell,
     future::{Future, IntoFuture},
-    io::Read,
-    pin::pin,
+    io::{Cursor, Read},
+    pin::{pin, Pin},
     rc::Rc,
     sync::Arc,
     task::{Context, Poll, Wake, Waker},
 };
+use tokio::io::{AsyncRead, AsyncReadExt};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // "Hello, 世界" with a u16 length header
     //
-    // From [INTRO(5)](http://man.cat-v.org/plan_9/5/intro):
-    //   Data items of larger or variable lengths are represented by a two-byte field specifying
-    //   a count, n, followed by n bytes of data. Text strings are represented this way, with
-    //   the text itself stored as a UTF-8 encoded sequence of Unicode charac- ters (see utf(6)).
-    //
-    //   Text strings in 9P messages are not NUL- terminated: n counts the bytes of UTF-8 data,
-    //   which include no final zero byte.  The NUL character is illegal in all text strings
-    //   in 9P, and is therefore excluded from file names, user names, and so on.
-    let data = &[
+    // In 9p, data items of larger or variable lengths are represented by a two-byte field
+    // specifying a count, n, followed by n bytes of data. Text strings are represented this way,
+    // with the text itself stored as a UTF-8 encoded sequence of Unicode characters without a
+    // trailing null byte.
+    let mut cur = Cursor::new(vec![
         0x0d, 0x00, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c,
-    ];
-    let r = StringReader::default();
-    let val = read_9p_sync_from_bytes(r, data);
+    ]);
 
-    println!("got val: {val:?}");
+    println!(">> reading using std::io::Read");
+    let s: String = read_9p_sync_from_bytes(&mut cur);
+    println!("  got val: {s:?}\n");
+
+    cur.set_position(0);
+
+    println!(">> reading using tokio::io::AsyncRead");
+    let s: String = read_9p_async_from_bytes(&mut cur).await;
+    println!("  got val: {s:?}");
+}
+
+fn read_9p_sync_from_bytes<T, R>(r: &mut R) -> T
+where
+    T: Read9p,
+    R: Read,
+{
+    let waker = Waker::from(Arc::new(StubWaker));
+    let mut context = Context::from_waker(&waker);
+    let s = State::default();
+
+    // SAFETY: assumes the impl of Read9p is a valid future for us to poll
+    let mut fut = unsafe { pin!(T::read(s.clone()).into_future()) };
+    loop {
+        match fut.as_mut().poll(&mut context) {
+            Poll::Ready(val) => return val,
+            Poll::Pending => {
+                let n = s.0.borrow().n;
+                println!("{n} bytes requested");
+                let mut buf = vec![0; n];
+                r.read_exact(&mut buf).unwrap();
+                s.0.borrow_mut().buf = Some(buf);
+            }
+        }
+    }
+}
+
+async fn read_9p_async_from_bytes<T, R>(r: &mut R) -> T
+where
+    T: Read9p,
+    R: AsyncRead + Unpin,
+{
+    let waker = Waker::from(Arc::new(StubWaker));
+    let mut context = Context::from_waker(&waker);
+    let s = State::default();
+
+    // SAFETY: assumes the impl of Read9p is a valid future for us to poll
+    let mut fut = unsafe { pin!(T::read(s.clone()).into_future()) };
+    loop {
+        match fut.as_mut().poll(&mut context) {
+            Poll::Ready(val) => return val,
+            Poll::Pending => {
+                let n = s.0.borrow().n;
+                println!("{n} bytes requested");
+                let mut buf = vec![0; n];
+                r.read_exact(&mut buf).await.unwrap();
+                s.0.borrow_mut().buf = Some(buf);
+            }
+        }
+    }
 }
 
 /// A no-op waker that is just used to create a [Context] in order to poll the [Read9p] future.
@@ -38,89 +91,59 @@ impl Wake for StubWaker {
     fn wake_by_ref(self: &Arc<Self>) {}
 }
 
-/// A real impl of a reader function would source the bytes here from IO rather than reading from a Vec.
-fn read_9p_sync_from_bytes<R: Read9p>(r: R, data: &[u8]) -> R::T {
-    let waker = Waker::from(Arc::new(StubWaker));
-    let mut context = Context::from_waker(&waker);
-    let mut offset = 0;
-
-    let mut s = r.state();
-    let mut fut = pin!(r.read().into_future());
-
-    loop {
-        match fut.as_mut().poll(&mut context) {
-            Poll::Ready(val) => {
-                return val;
-            }
-            Poll::Pending => {
-                let n = s.bytes_needed();
-                println!("{n} bytes requested");
-                s.set_buf(data[offset..offset + n].to_vec());
-                offset += n;
-            }
+/// Helper struct for awaiting a Future that returns pending once so we can return control to the
+/// poll loop and perform IO.
+struct Yield(bool);
+impl Future for Yield {
+    type Output = ();
+    fn poll(mut self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<()> {
+        if self.0 {
+            Poll::Ready(())
+        } else {
+            self.0 = true;
+            Poll::Pending
         }
     }
 }
 
 #[derive(Default, Debug, Clone)]
 struct State(Rc<RefCell<StateInner>>);
-impl State {
-    fn request_bytes(&mut self, n: usize) {
-        self.0.borrow_mut().n = n;
-    }
-
-    fn bytes_needed(&self) -> usize {
-        self.0.borrow().n
-    }
-
-    fn set_buf(&mut self, buf: Vec<u8>) {
-        self.0.borrow_mut().buf = buf;
-    }
-
-    fn buf(&self) -> Vec<u8> {
-        self.0.borrow().buf.clone()
-    }
-}
 
 #[derive(Default, Debug)]
 struct StateInner {
     n: usize,
-    buf: Vec<u8>,
+    buf: Option<Vec<u8>>,
+}
+
+/// Request a specific number of bytes from the parent poll loop and then yield to that poll loop
+/// so it can perform IO and provide the requested data.
+macro_rules! request_bytes {
+    ($s:expr, $n:expr) => {{
+        $s.0.borrow_mut().n = $n;
+        Yield(false).await;
+        $s.0.borrow_mut().buf.take().unwrap()
+    }};
+}
+
+/// # Safety
+/// The read method of this trait requires that you only yield view the [request_bytes] macro.
+#[allow(async_fn_in_trait)]
+unsafe trait Read9p {
+    /// # Safety
+    /// Implementations of `read` need to ensure that the only await points they contain are
+    /// from calls to the [request_bytes] macro.
+    async unsafe fn read(state: State) -> Self;
 }
 
 #[allow(async_fn_in_trait)]
-trait Read9p {
-    type T;
-
-    fn state(&self) -> State;
-    async fn read(self) -> Self::T;
-}
-
-#[derive(Default)]
-struct StringReader {
-    s: State,
-}
-
-impl Read9p for StringReader {
-    type T = String;
-
-    fn state(&self) -> State {
-        self.s.clone()
-    }
-
-    async fn read(mut self) -> String {
+unsafe impl Read9p for String {
+    async unsafe fn read(state: State) -> Self {
         let n = size_of::<u16>();
-        self.s.request_bytes(n);
-        pending!();
-
-        let data = self.s.buf()[0..n].try_into().unwrap();
+        let buf = request_bytes!(state, n);
+        let data = buf[0..n].try_into().unwrap();
         let len = u16::from_le_bytes(data) as usize;
-        self.s.request_bytes(len);
-        pending!();
+        let buf = request_bytes!(state, len);
 
-        let mut s = String::with_capacity(len);
-        self.s.buf().as_slice().read_to_string(&mut s).unwrap();
-
-        s
+        String::from_utf8(buf).unwrap()
     }
 }
