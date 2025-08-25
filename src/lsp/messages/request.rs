507a8488d80d198b7f1dff33f201ecb6b4507d9e
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-08-25 17:03:16 +0100

    Add support for requesting LSP completion

--- src/lsp/messages/request.rs
+++ src/lsp/messages/request.rs
@@ -1,8 +1,9 @@
 use crate::{
     buffer::Buffers,
+    die,
     editor::{Action, Actions, MbSelect, MbSelector, MiniBufferSelection, ViewPort},
     lsp::{
-        LSP_FILE, LspManager, Pending, PendingParams, PendingRequest, Pos, PositionEncoding,
+        LSP_FILE, LspManager, Pending, PendingParams, PendingRequest, Pos, PositionEncoding, Req,
         capabilities::{Capabilities, Coords},
         client::Status,
         messages::{LspNotification, txtdoc_pos},
@@ -12,7 +13,7 @@ use crate::{
 use lsp_types::{
     DynamicRegistrationClientCapabilities, GotoDefinitionParams, GotoDefinitionResponse, Location,
 };
-use std::{borrow::Cow, process, str::FromStr};
+use std::{borrow::Cow, process, str::FromStr, sync::mpsc::Sender};
 use tracing::{debug, error};
 
 /// Outgoing requests from us to the server that we will need to handle responses for
@@ -126,10 +127,11 @@ impl LspRequest for lsp_types::request::Initialize {
 
     fn prepare((root, initialization_options): Self::Data) -> Self::Params {
         use lsp_types::{
-            ClientCapabilities, GeneralClientCapabilities, HoverClientCapabilities,
-            InitializeParams, MarkupKind, NumberOrString, PositionEncodingKind,
-            TextDocumentClientCapabilities, Uri, WindowClientCapabilities, WorkDoneProgressParams,
-            WorkspaceClientCapabilities, WorkspaceFolder,
+            ClientCapabilities, CompletionClientCapabilities, CompletionItemCapability,
+            GeneralClientCapabilities, HoverClientCapabilities, InitializeParams, MarkupKind,
+            NumberOrString, PositionEncodingKind, TextDocumentClientCapabilities, Uri,
+            WindowClientCapabilities, WorkDoneProgressParams, WorkspaceClientCapabilities,
+            WorkspaceFolder,
         };
 
         let basename = root.split("/").last().unwrap_or_default();
@@ -161,6 +163,29 @@ impl LspRequest for lsp_types::request::Initialize {
                     hover: Some(HoverClientCapabilities {
                         dynamic_registration: Some(true),
                         content_format: Some(vec![MarkupKind::PlainText]),
+                    }),
+                    completion: Some(CompletionClientCapabilities {
+                        dynamic_registration: Some(true),
+                        completion_item: Some(CompletionItemCapability {
+                            snippet_support: None,
+                            commit_characters_support: None,
+                            documentation_format: Some(vec![MarkupKind::PlainText]),
+                            deprecated_support: None,
+                            preselect_support: None,
+                            tag_support: None,
+                            insert_replace_support: Some(false),
+                            resolve_support: Some(
+                                lsp_types::CompletionItemCapabilityResolveSupport {
+                                    properties: vec!["additionalTextEdits".to_string()],
+                                },
+                            ),
+                            insert_text_mode_support: None,
+                            label_details_support: None,
+                        }),
+                        completion_item_kind: Some(Default::default()),
+                        context_support: Some(true),
+                        insert_text_mode: None,
+                        completion_list: None,
                     }),
                     // https://docs.rs/lsp-types/0.97.0/lsp_types/struct.TextDocumentClientCapabilities.html
                     ..Default::default()
@@ -531,5 +556,282 @@ impl MbSelect for References {
 
             _ => None,
         }
+    }
+}
+
+// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_completion
+impl LspRequest for lsp_types::request::Completion {
+    type Pending = ();
+    type Data = Pos;
+
+    fn prepare(
+        Pos {
+            file,
+            line,
+            character,
+        }: Self::Data,
+    ) -> Self::Params {
+        use lsp_types::{CompletionContext, CompletionParams, CompletionTriggerKind};
+
+        CompletionParams {
+            text_document_position: txtdoc_pos(&file, line, character),
+            work_done_progress_params: Default::default(),
+            partial_result_params: Default::default(),
+            context: Some(CompletionContext {
+                trigger_kind: CompletionTriggerKind::INVOKED,
+                trigger_character: None,
+            }),
+        }
+    }
+
+    fn pending(_: Self::Pending) -> Pending {
+        Pending::Completion
+    }
+
+    fn handle_res(
+        lsp_id: usize,
+        resp: Option<lsp_types::CompletionResponse>,
+        _: (),
+        man: &mut LspManager,
+    ) -> Option<Actions> {
+        let enc = match man.clients.get_mut(&lsp_id) {
+            Some(client) => client.position_encoding,
+            None => {
+                man.send_status("no attached LSP client".to_string());
+                return None;
+            }
+        };
+
+        let items = match resp? {
+            lsp_types::CompletionResponse::List(l) => l.items,
+            lsp_types::CompletionResponse::Array(items) => items,
+        };
+
+        let completions: Vec<_> = items
+            .into_iter()
+            .map(|item| Completion::new(item, enc, lsp_id, &man.tx_req))
+            .collect();
+
+        Some(Actions::Single(Action::MbSelect(
+            Completions(completions).into_selector(),
+        )))
+    }
+}
+
+// We assume that each TextEdit is a single edit to the current buffer.
+fn actions_from_text_edit(
+    edit: &lsp_types::TextEdit,
+    enc: PositionEncoding,
+    use_xdot: bool,
+) -> [Action; 2] {
+    let coords = Coords::new_from_range(edit.range, enc);
+
+    if use_xdot {
+        [
+            Action::XDotSetFromCoords { coords },
+            Action::XInsertString {
+                s: edit.new_text.clone(),
+            },
+        ]
+    } else {
+        [
+            Action::DotSetFromCoords { coords },
+            Action::InsertString {
+                s: edit.new_text.clone(),
+            },
+        ]
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Completion {
+    comp_item: lsp_types::CompletionItem,
+    edit_actions: Option<Actions>,
+    resolve_args: Option<(usize, Sender<Req>)>,
+    kind: String,
+}
+
+impl Completion {
+    fn new(
+        comp_item: lsp_types::CompletionItem,
+        enc: PositionEncoding,
+        lsp_id: usize,
+        tx_req: &Sender<Req>,
+    ) -> Self {
+        let kind = comp_item.kind.map(|k| format!("{k:?}")).unwrap_or_default();
+        let (resolve_args, edit_actions) = match comp_item.text_edit.as_ref() {
+            Some(lsp_types::CompletionTextEdit::Edit(edit)) => {
+                // Some LSP servers will defer computing data for additional edits until explicitly
+                // requested by the client using completionItem/resolve. If the "data" field is
+                // non-null then this is an indicator that we need to make the resolve request (the
+                // data inside of the "data" field itself is only intended for use by the LSP
+                // server itself in order to resolve the request so we shouldn't be doing anything
+                // with it other than passing it back).
+                if comp_item.data.is_some() {
+                    (Some((lsp_id, tx_req.clone())), None)
+                } else {
+                    (
+                        None,
+                        Some(Actions::Multi(
+                            actions_from_text_edit(edit, enc, false).to_vec(),
+                        )),
+                    )
+                }
+            }
+
+            Some(lsp_types::CompletionTextEdit::InsertAndReplace(_)) | None => (None, None),
+        };
+
+        Self {
+            comp_item,
+            edit_actions,
+            resolve_args,
+            kind,
+        }
+    }
+
+    // From the LSP spec docs on the insertText field:
+    // 	 A string that should be inserted into a document when selecting
+    //   this completion. When omitted the label is used as the insert text
+    //   for this item.
+    fn raw_insert_text(&self) -> String {
+        self.comp_item
+            .insert_text
+            .clone()
+            .unwrap_or_else(|| self.comp_item.label.clone())
+    }
+
+    fn mb_line(&self, label_width: usize, kind_width: usize) -> String {
+        format!(
+            "{:<label_width$} {:<kind_width$}  {}",
+            self.comp_item.label,
+            self.kind,
+            self.comp_item.detail.as_deref().unwrap_or_default(),
+            label_width = label_width,
+            kind_width = kind_width
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Completions(Vec<Completion>);
+
+impl MbSelect for Completions {
+    fn clone_selector(&self) -> MbSelector {
+        self.clone().into_selector()
+    }
+
+    /// The initial filter input we want is the "word" so far under the cursor.
+    /// We move back a character before expanding in order to use the last character typed rather
+    /// than the current insert position.
+    ///
+    /// > This is a little funky with completions at word boundaries.
+    fn initial_input(&self, buffers: &Buffers) -> Option<String> {
+        let b = buffers.active();
+        let mut cur = b.dot.active_cur();
+        cur.idx = cur.idx.saturating_sub(1);
+
+        let input = buffers.active().word_under_dot(cur.into());
+
+        match input.chars().next() {
+            Some(ch) if ch.is_alphanumeric() => Some(input),
+            _ => None,
+        }
+    }
+
+    fn prompt_and_options(&self, _buffers: &Buffers) -> (String, Vec<String>) {
+        let label_width = self
+            .0
+            .iter()
+            .map(|c| c.comp_item.label.chars().count())
+            .max()
+            .unwrap_or_default();
+        let kind_width = self
+            .0
+            .iter()
+            .map(|c| c.kind.chars().count())
+            .max()
+            .unwrap_or_default();
+
+        (
+            "Completions> ".to_owned(),
+            self.0
+                .iter()
+                .map(|c| c.mb_line(label_width, kind_width))
+                .collect(),
+        )
+    }
+
+    fn selected_actions(&self, sel: MiniBufferSelection) -> Option<Actions> {
+        match sel {
+            MiniBufferSelection::Line { cy, .. } => self.0.get(cy).and_then(|c| {
+                if let Some((lsp_id, tx_req)) = c.resolve_args.clone() {
+                    let pending =
+                        PendingParams::ResolveCompletionItem(Box::new(c.comp_item.clone()));
+                    let req = Req::Pending(PendingRequest { lsp_id, pending });
+                    if let Err(e) = tx_req.send(req) {
+                        die!("LSP manager died: {e}")
+                    }
+
+                    None
+                } else {
+                    c.edit_actions.clone().or_else(|| {
+                        Some(Actions::Single(Action::InsertString {
+                            s: c.raw_insert_text(),
+                        }))
+                    })
+                }
+            }),
+
+            _ => None,
+        }
+    }
+}
+
+// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#completionItem_resolve
+//
+// We need to resolve both the completion itself and any additional edits
+impl LspRequest for lsp_types::request::ResolveCompletionItem {
+    type Pending = ();
+    type Data = Box<lsp_types::CompletionItem>;
+
+    fn prepare(item: Self::Data) -> Self::Params {
+        *item
+    }
+
+    fn pending(_: Self::Pending) -> Pending {
+        Pending::ResolveCompletionItem
+    }
+
+    fn handle_res(
+        lsp_id: usize,
+        comp_item: lsp_types::CompletionItem,
+        _: (),
+        man: &mut LspManager,
+    ) -> Option<Actions> {
+        let enc = match man.clients.get_mut(&lsp_id) {
+            Some(client) => client.position_encoding,
+            None => {
+                man.send_status("no attached LSP client".to_string());
+                return None;
+            }
+        };
+
+        let mut edit_actions = match comp_item.text_edit.as_ref() {
+            Some(lsp_types::CompletionTextEdit::Edit(edit)) => {
+                actions_from_text_edit(edit, enc, false).to_vec()
+            }
+            Some(lsp_types::CompletionTextEdit::InsertAndReplace(_)) | None => Vec::new(),
+        };
+
+        edit_actions.extend(
+            comp_item
+                .additional_text_edits
+                .unwrap_or_default()
+                .iter()
+                .flat_map(|te| actions_from_text_edit(te, enc, true)),
+        );
+
+        Some(Actions::Multi(edit_actions))
     }
 }
