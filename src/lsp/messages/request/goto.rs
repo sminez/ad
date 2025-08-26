6b3985b4deb3542d82e923afb6b99a0cfe79c7ed
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-08-26 07:53:18 +0100

    Use a macro for defining impl of LSP Goto requests

--- src/lsp/messages/request/goto.rs
+++ src/lsp/messages/request/goto.rs
@@ -12,110 +12,64 @@ use lsp_types::{
 };
 use tracing::error;
 
-impl LspRequest for GotoDeclaration {
-    type Pending = ();
-    type Data = Pos;
+// The types being used here look a little odd at first glance but the lsp_types crate reuses the
+// same params and response types for each of the Goto* requests as they have the same shape.
+macro_rules! impl_goto_req {
+    ($req_ty:ident) => {
+        impl LspRequest for $req_ty {
+            type Pending = ();
+            type Data = Pos;
 
-    fn prepare(data: Self::Data) -> Self::Params {
-        pos_to_params(data)
-    }
-
-    fn pending(_: Self::Pending) -> Pending {
-        Pending::GotoDeclaration
-    }
-
-    fn handle_res(
-        lsp_id: usize,
-        params: Option<GotoDefinitionResponse>,
-        _: Self::Pending,
-        man: &mut LspManager,
-    ) -> Option<Actions> {
-        handle_goto_response(lsp_id, params, man)
-    }
-}
-
-impl LspRequest for GotoDefinition {
-    type Pending = ();
-    type Data = Pos;
-
-    fn prepare(data: Self::Data) -> Self::Params {
-        pos_to_params(data)
-    }
-
-    fn pending(_: Self::Pending) -> Pending {
-        Pending::GotoDefinition
-    }
-
-    fn handle_res(
-        lsp_id: usize,
-        params: Option<GotoDefinitionResponse>,
-        _: Self::Pending,
-        man: &mut LspManager,
-    ) -> Option<Actions> {
-        handle_goto_response(lsp_id, params, man)
-    }
-}
-
-impl LspRequest for GotoTypeDefinition {
-    type Pending = ();
-    type Data = Pos;
-
-    fn prepare(data: Self::Data) -> Self::Params {
-        pos_to_params(data)
-    }
-
-    fn pending(_: Self::Pending) -> Pending {
-        Pending::GotoTypeDefinition
-    }
-
-    fn handle_res(
-        lsp_id: usize,
-        params: Option<GotoDefinitionResponse>,
-        _: Self::Pending,
-        man: &mut LspManager,
-    ) -> Option<Actions> {
-        handle_goto_response(lsp_id, params, man)
-    }
-}
-
-fn handle_goto_response(
-    lsp_id: usize,
-    params: Option<GotoDefinitionResponse>,
-    man: &mut LspManager,
-) -> Option<Actions> {
-    let enc = man.clients.get(&lsp_id)?.position_encoding;
-
-    let (path, coords) = match params? {
-        GotoDefinitionResponse::Scalar(loc) => Coords::new(loc, enc),
-        GotoDefinitionResponse::Array(mut locs) => {
-            if locs.is_empty() {
-                return None;
+            fn prepare(
+                Pos {
+                    file,
+                    line,
+                    character,
+                }: Pos,
+            ) -> Self::Params {
+                GotoDefinitionParams {
+                    text_document_position_params: txtdoc_pos(&file, line, character),
+                    work_done_progress_params: Default::default(),
+                    partial_result_params: Default::default(),
+                }
             }
-            Coords::new(locs.remove(0), enc)
-        }
-        GotoDefinitionResponse::Link(links) => {
-            error!("unhandled goto definition links response: {links:?}");
-            return None;
+
+            fn pending(_: Self::Pending) -> Pending {
+                Pending::$req_ty
+            }
+
+            fn handle_res(
+                lsp_id: usize,
+                params: Option<GotoDefinitionResponse>,
+                _: Self::Pending,
+                man: &mut LspManager,
+            ) -> Option<Actions> {
+                let enc = man.clients.get(&lsp_id)?.position_encoding;
+
+                let (path, coords) = match params? {
+                    GotoDefinitionResponse::Scalar(loc) => Coords::new(loc, enc),
+                    GotoDefinitionResponse::Array(mut locs) => {
+                        if locs.is_empty() {
+                            return None;
+                        }
+                        Coords::new(locs.remove(0), enc)
+                    }
+                    GotoDefinitionResponse::Link(links) => {
+                        error!("unhandled goto definition links response: {links:?}");
+                        return None;
+                    }
+                };
+
+                Some(Actions::Multi(vec![
+                    Action::OpenFile { path },
+                    Action::DotSetFromCoords { coords },
+                    Action::SetViewPort(ViewPort::Center),
+                ]))
+            }
         }
     };
-
-    Some(Actions::Multi(vec![
-        Action::OpenFile { path },
-        Action::DotSetFromCoords { coords },
-        Action::SetViewPort(ViewPort::Center),
-    ]))
 }
 
-fn pos_to_params(
-    Pos {
-        file,
-        line,
-        character,
-    }: Pos,
-) -> GotoDefinitionParams {
-    GotoDefinitionParams {
-        text_document_position_params: txtdoc_pos(&file, line, character),
-        work_done_progress_params: Default::default(),
-        partial_result_params: Default::default(),
-    }
-}
+impl_goto_req!(GotoDeclaration);
+impl_goto_req!(GotoDefinition);
+impl_goto_req!(GotoTypeDefinition);
