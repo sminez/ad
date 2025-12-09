80c4d1f531159f68baddc15d53744e57497ab97d
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-12-09 08:45:10 +0000

    Iterate over file lines in -9p read rather than a single blocking read

--- src/main.rs
+++ src/main.rs
@@ -143,7 +143,11 @@ fn run_9p(aname: String, action: Cmd9p, path: String) {
 
 fn run_9p_command(action: Cmd9p, path: &str, mut client: UnixClient) -> io::Result<()> {
     match action {
-        Cmd9p::Read => print!("{}", client.read_str(path)?),
+        Cmd9p::Read => {
+            for line in client.iter_lines(path)? {
+                println!("{line}");
+            }
+        }
 
         Cmd9p::Write => {
             let mut content = String::new();
